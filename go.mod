b5110b5040aaa227dd934730622c4a79098a700c
Yash <yashsaini18166@gmail.com>
2024-07-13 15:11:44 +0530

    cors update

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/cors v1.11.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Yashh56/HotelHub/routes"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -39,9 +40,16 @@ func main() {
 	routes.PaymentRoutes(r, client)
 	routes.ReviewRoutes(r, client)
 
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "https://your-frontend-domain.com"}, // Update with your frontend URL
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	}).Handler(r)
+
 	server := &http.Server{
 		Addr:           ":" + port,
-		Handler:        r,
+		Handler:        corsHandler,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
